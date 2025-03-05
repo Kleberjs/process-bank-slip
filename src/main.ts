88a8c48795dd2831839ed688d1217e867efa6af6
Kleberjs <kleberti07@gmail.com>
2025-03-05 11:16:56 -0300

    chore(swagger): config swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  // swagger configuration
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('ms-api-file')
+    .setDescription('Upload csv file to generate bank slip')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup(`api/docs`, app, document);
+  // end swagger configuration
+
+  await app.listen(port);
 }
-bootstrap();
+bootstrap().catch((error) =>
+  console.log(`Error boostrap ${JSON.stringify(error)}`),
+);
